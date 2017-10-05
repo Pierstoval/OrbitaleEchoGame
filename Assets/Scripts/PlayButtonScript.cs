a4e77210183ea45bfd4979959fa81e6008bb459f
Alex Rock Ancelet <pierstoval@gmail.com>
2017-10-06 00:12:52 +0200

    Added main menu with play button and tutorial scenes

---
+++ Assets/Scripts/PlayButtonScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonScript : MonoBehaviour {
+	public void LoadPlayScene(string levelToLoad = "Tutorial") {
+		SceneManager.LoadScene("Tutorial");
+	}
+}
