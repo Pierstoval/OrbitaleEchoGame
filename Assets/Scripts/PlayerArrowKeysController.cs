eed5793c444de8147ac417fcf1a6dc7dac822063
Alex Rock Ancelet <pierstoval@gmail.com>
2017-10-06 21:30:25 +0200

    Enhance player movement

--- Assets/Scripts/PlayerArrowKeysController.cs
+++ Assets/Scripts/PlayerArrowKeysController.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+public class PlayerArrowKeysController : MonoBehaviour
 {
-	public float acceleration = 1f;
-	public float maxSpeed = 0.1f;
+	public float acceleration = 20;
+	public float maxSpeed = 3;
 
 	private Rigidbody2D playerRigidBody;
 	private int count;
@@ -15,6 +15,10 @@ public class PlayerController : MonoBehaviour
 	{
 		playerRigidBody = GetComponent<Rigidbody2D>();
 		count = 0;
+	}
+
+	void Update () 
+	{
 	}
 
 	void FixedUpdate ()
@@ -33,10 +37,16 @@ public class PlayerController : MonoBehaviour
 		Vector2 velocity = playerRigidBody.velocity;
 		Vector2 pos = transform.position;
 
-		if (velocity.magnitude > maxSpeed) {
-			playerRigidBody.velocity = Vector2.ClampMagnitude (velocity, maxSpeed);
-		} else {
+		//if (velocity.magnitude > maxSpeed) {
+		//	playerRigidBody.velocity = Vector2.ClampMagnitude (velocity, maxSpeed);
+		//} else {
 			playerRigidBody.AddForce (movement * acceleration);
+		//}
+
+		Vector2 moveDirection = playerRigidBody.velocity;
+		if (moveDirection != Vector2.zero) {
+			float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 		}
 	}
 
