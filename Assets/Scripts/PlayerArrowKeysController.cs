a7c2d2cb86b10ba4c566bd4a94ed3df14823e31e
Alex Rock Ancelet <pierstoval@gmail.com>
2017-10-07 13:10:38 +0200

    Make light stop when too slow

--- Assets/Scripts/PlayerArrowKeysController.cs
+++ Assets/Scripts/PlayerArrowKeysController.cs
@@ -7,6 +7,7 @@ public class PlayerArrowKeysController : MonoBehaviour
 {
 	public float acceleration = 20;
 	public float maxSpeed = 3;
+	public float minSpeedToStop = 0.1f;
 
 	private Rigidbody2D playerRigidBody;
 	private int count;
@@ -17,36 +18,23 @@ public class PlayerArrowKeysController : MonoBehaviour
 		count = 0;
 	}
 
-	void Update () 
-	{
-	}
-
 	void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		Debug.Log (
-			"x:"+playerRigidBody.velocity.x
-			+"\ty:"+playerRigidBody.velocity.y
-			+"\tmagnitude:"+playerRigidBody.velocity.magnitude
-			+"\tnormalized:"+playerRigidBody.velocity.normalized
-		);
-
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		Vector2 velocity = playerRigidBody.velocity;
 		Vector2 pos = transform.position;
 
-		//if (velocity.magnitude > maxSpeed) {
-		//	playerRigidBody.velocity = Vector2.ClampMagnitude (velocity, maxSpeed);
-		//} else {
-			playerRigidBody.AddForce (movement * acceleration);
-		//}
+		playerRigidBody.AddForce (movement * acceleration);
 
 		Vector2 moveDirection = playerRigidBody.velocity;
-		if (moveDirection != Vector2.zero) {
-			float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		if (moveDirection.magnitude > minSpeedToStop) {
+			float angle = Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+		} else {
+			playerRigidBody.velocity = Vector2.zero;
 		}
 	}
 
