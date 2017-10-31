0ae900bb46e5cb98e25a1fbb3dc5123691ed71f4
Alex Rock Ancelet <pierstoval@gmail.com>
2017-10-31 16:40:53 +0100

    Prepare note activation interaction

---
+++ Assets/Scripts/PlayerClickController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClickController : MonoBehaviour
+{
+    private CircleOnClickAnimation animation;
+
+    void Start ()
+    {
+        animation = GetComponent<CircleOnClickAnimation> ();
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown (KeyCode.F)) {
+            Debug.Log ("\"F\" key was pressed");
+        }
+    }
+}
